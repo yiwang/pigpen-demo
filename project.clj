b2c693582abdfdbe125775bfe10c6c90546050ac
Yi Wang <e@yi-wang.me>
2016-02-04 14:46:35 -0500

    add graph-viz

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.netflix.pigpen/pigpen-pig "0.3.1"]]
+                 [com.netflix.pigpen/pigpen-pig "0.3.1"]
+                 [com.netflix.pigpen/pigpen-viz "0.3.1"]]
   :profiles {:dev {:dependencies [[org.apache.pig/pig "0.13.0"]
                                   [org.apache.hadoop/hadoop-core "1.1.2"]]}}
                  )
