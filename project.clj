efd5d6cf6c601cd9d413cd285f348d2a0a91bec3
Yi Wang <e@yi-wang.me>
2016-02-04 10:45:39 -0500

    project.clj

--- project.clj
+++ project.clj
@@ -3,4 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [com.netflix.pigpen/pigpen-pig "0.3.1"]]
+  :profiles {:dev {:dependencies [[org.apache.pig/pig "0.13.0"]
+                                  [org.apache.hadoop/hadoop-core "1.1.2"]]}}
+                 )
+
+
